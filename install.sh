b74d08f54c1f51af5cb7092a27fa0791dad3fb52
Argiris Ntalianis <arigirisntalianis@gmail.com>
2017-01-31 00:21:16 +0200

    Add install wrapper script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install:
+#   awesome window manager
+#   Ctags
+#   tig
+#   xscreensaver
+#   xbacklight
+#   zsh
+#   pip
+#   vim
+#   xclip
+#   ruby dependencies
+#   terminator
+#   shutter
+#   blueman
+#   openvpn
+#   ripgrep
+apt-get install -y awesome awesome-extra exuberant-ctags tig xscreensaver xbacklight zsh python-pip vim-gtk xclip libssl1.0-dev libreadline-dev zlib1g-dev acpi terminator shutter blueman openvpn ripgrep
